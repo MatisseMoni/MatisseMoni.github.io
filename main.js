d16691ba5ad95cfc4f3484b1230c07bd92717ce7
Matisse Moni <matisse.moni@etu.unice.fr>
2022-02-21 17:28:47 +0100

    test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', () => {
     if ("serviceWorker" in navigator)
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/service-worker.js')
 })
