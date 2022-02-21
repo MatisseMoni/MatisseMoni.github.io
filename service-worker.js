334710bf300e2ea318496d40421413231df4a22c
Matisse Moni <matisse.moni@etu.unice.fr>
2022-02-21 17:11:34 +0100

    modif infra

--- service-worker.js
+++ service-worker.js
@@ -8,3 +8,18 @@ self.addEventListener('install', function(event) { // Installation
             return cache.addAll(urlsToCache); // Met en cache
         }));
 });
+
+self.addEventListener('fetch', evt => { // Sur une requête (avec ou sans la méthode fetch)
+    evt.respondWith( // On répond à la requête avec …
+        fetch(evt.request).then(newResponse => { // Si on le trouve
+            console.log("From network");
+            caches.open(cacheName).then(cache => cache.put(evt.request, newResponse));
+            return newResponse.clone(); // On retourne ce qui est dans le cache
+        }).catch(function(error) {
+            return caches.match(evt.request).then(res => { // On cherche dans le cache
+                // Si on le trouve
+                console.log("From cache");
+                return res; // On retourne ce qui est dans le cache);
+            });
+        }));
+});
